f40a9c7dc9cb8286af1add9f52cec321bf580dec
Diego Estrada <35475066+diegoestradaXO@users.noreply.github.com>
2021-04-15 19:17:36 -0600

    Variables dicotómicas

--- HDT6.R
+++ HDT6.R
@@ -59,3 +59,25 @@ scatter.smooth(train$YearRemodAdd, train$SalePrice)
 scatter.smooth(train$TotRmsAbvGrd, train$SalePrice)
 scatter.smooth(train$MoSold, train$SalePrice)
 scatter.smooth(train$OverallQual, train$SalePrice)
+
+#variables dicotomicas
+datos$grupo <- ifelse(datos$SalePrice<178000, "3", 
+                      ifelse(datos$SalePrice<301000, "2",
+                             ifelse(datos$SalePrice<756000,"1",NA)))
+
+datos$grupo2 <- ifelse(datos$SalePrice<178000, "3", 
+                      ifelse(datos$SalePrice<301000, "2",NA))
+
+datos$grupo3 <- ifelse(datos$SalePrice<178000, "3", NA)
+
+datos$grupo <- as.factor(datos$grupo)
+datos$grupo2 <- as.factor(datos$grupo2)
+datos$grupo3 <- as.factor(datos$grupo3)
+
+datos<-cbind(datos,dummy(datos$grupo,verbose = T),dummy(datos$grupo2,verbose = T), dummy(datos$grupo3,verbose = T))
+
+datos <- datos[,c("LotFrontage","LotArea","GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","X1stFlrSF","GarageYrBlt","GarageArea","YearRemodAdd", "SalePrice", "datos1","datos2","datos3")]
+
+datos <- na.omit(datos)
+
+head(datos, 30)
